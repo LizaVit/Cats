51fc98876260b1afe6e82b7d4207e665f8cdd2d4
Liza Vitkovska <lizavetavitkovkaja@gmail.com>
2023-06-26 10:26:45 +0300

    HW 8 with 2 options

--- HW 8.py
+++ HW 8.py
@@ -1,8 +1,8 @@
 def cats():
-    cats_list = [False for i in range(0, 10)]
+    cats_list = [False for i in range(0, 100)]
     cats_game = []
-    for step in range(1, 11):
-        for i in range(step-1, 10, step):
+    for step in range(1, 101):
+        for i in range(step-1, 100, step):
             cats_list[i] = not cats_list[i]
             print(step, i)
         print(list(enumerate(cats_list)))
@@ -14,5 +14,21 @@ def cats():
 
 cats()
                 
+#option 2
 
-#test
+def cats():
+    cats_list = list(range(1, 101))
+    cats_list_game = []
+    for i in cats_list:
+        particular_cat = False
+        for step in range(1, i+1):
+            if i % step == 0:
+                particular_cat = not particular_cat
+        cats_list_game.append(particular_cat)
+    cats_with_hats = []
+    for i, j in enumerate(cats_list_game, start=1):
+        if j == True:
+            cats_with_hats.append(i)
+    print(f'This cats are with hats {cats_with_hats}')
+
+cats()
