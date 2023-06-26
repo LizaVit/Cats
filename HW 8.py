30a93319f4ce091424a588a684dafb443be84d10
Liza Vitkovska <lizavetavitkovkaja@gmail.com>
2023-06-26 09:46:50 +0300

    Initial commit Cats

---
+++ HW 8.py
@@ -0,0 +1,16 @@
+def cats():
+    cats_list = [False for i in range(0, 10)]
+    cats_game = []
+    for step in range(1, 11):
+        for i in range(step-1, 10, step):
+            cats_list[i] = not cats_list[i]
+            print(step, i)
+        print(list(enumerate(cats_list)))
+    for i, j in enumerate(cats_list):
+        if j:
+            cats_game.append(i+1)       
+    print(f'This cats are with hats {cats_game}')
+
+
+cats()
+                
